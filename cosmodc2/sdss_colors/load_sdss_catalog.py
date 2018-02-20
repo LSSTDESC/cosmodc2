974d7a7e6e568870b65cfab547cc151075d945b4
Andrew Hearin <andrew.hearin@yale.edu>
2018-02-20 13:44:54 -0600

    Updated sdss catalog to load with value-added and renamed columns

--- cosmodc2/sdss_colors/load_sdss_catalog.py
+++ cosmodc2/sdss_colors/load_sdss_catalog.py
@@ -1,6 +1,7 @@
 """
 """
 from astropy.table import Table
+from halotools.utils import sliding_conditional_percentile
 
 
 default_fname = "/Users/aphearin/Dropbox/protoDC2/SDSS/dr10_mgs_colors_processed.txt"
@@ -42,4 +43,21 @@ def load_umachine_processed_sdss_catalog(fname=default_fname):
     assuming h=0.7 as M*[h=0.7]. Then we have: M*[h=1] = h*h*M*[h=0.7].
     The stellar masses appearing in the returned catalog are M*[h=0.7].
     """
-    return Table.read(fname, format='ascii.commented_header')
+    t = Table.read(fname, format='ascii.commented_header')
+    for key in t.keys():
+        t.rename_column(key, key.lower())
+    t.rename_column('ricolor', 'restframe_extincted_sdss_ri')
+    t.rename_column('grcolor', 'restframe_extincted_sdss_gr')
+    t.rename_column('rmag', 'restframe_extincted_sdss_abs_magr')
+    t.rename_column('size[kpc]', 'size_kpc')
+    t.rename_column('dr7photoobjid', 'dr7_PhotoObj_Table_ID')
+
+    t['sfr_percentile_fixed_sm'] = sliding_conditional_percentile(
+            t['sm'], t['ssfr'], 301)
+    t['gr_percentile_fixed_magr'] = sliding_conditional_percentile(
+            t['restframe_extincted_sdss_abs_magr'], t['restframe_extincted_sdss_gr'], 301)
+
+    return t
+
+
+
