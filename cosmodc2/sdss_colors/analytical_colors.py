c4ad82d873995d66e87a0a21d8b080227f83ecfe
Andrew Hearin <andrew.hearin@yale.edu>
2018-03-28 13:20:54 -0500

    Migrated code into analytical_colors module. Retuned endpoint behavior for gentler extrapolation beyond abscissa

--- cosmodc2/sdss_colors/analytical_colors.py
+++ cosmodc2/sdss_colors/analytical_colors.py
@@ -4,9 +4,9 @@ import numpy as np
 from astropy.utils.misc import NumpyRNGContext
 
 
-def red_sequence_width_gr(magr,
-        x=[-22.5, -21, -20, -18, -15],
-        y=[0.05, 0.06, 0.065, 0.06, 0.06]):
+def sequence_width(magr, x, y):
+    """
+    """
     c2, c1, c0 = np.polyfit(x, y, deg=2)
     result = c0 + c1*magr + c2*magr**2
 
@@ -17,38 +17,45 @@ def red_sequence_width_gr(magr,
     result = np.where(magr > xmax, faint_edge_value, result)
     result = np.where(magr < xmin, bright_edge_value, result)
     return result
+
+
+def sequence_peak(magr, x, y):
+    """
+    """
+    c2, c1, c0 = np.polyfit(x, np.log(y), deg=2)
+    result = np.exp(c0 + c1*magr + c2*magr**2)
+
+    #  Now clip endpoints
+    xmin, xmax = np.min(x), np.max(x)
+    faint_edge_value = np.exp(c0 + c1*xmax + c2*xmax**2)
+    bright_edge_value = np.exp(c0 + c1*xmin + c2*xmin**2)
+    result = np.where(magr > xmax, faint_edge_value, result)
+    result = np.where(magr < xmin, bright_edge_value, result)
+    return result
+
+
+def red_sequence_width_gr(magr,
+        x=[-22.5, -21, -20, -18, -15],
+        y=[0.05, 0.06, 0.065, 0.06, 0.06]):
+    return sequence_width(magr, x, y)
 
 
 def main_sequence_width_gr(magr,
         x=[-22.5, -21, -20, -18, -15],
         y=[0.1, 0.1, 0.1, 0.1, 0.1]):
-    c2, c1, c0 = np.polyfit(x, y, deg=2)
-    result = c0 + c1*magr + c2*magr**2
-
-    #  Now clip endpoints
-    xmin, xmax = np.min(x), np.max(x)
-    faint_edge_value = c0 + c1*xmax + c2*xmax**2
-    bright_edge_value = c0 + c1*xmin + c2*xmin**2
-    result = np.where(magr > xmax, faint_edge_value, result)
-    result = np.where(magr < xmin, bright_edge_value, result)
-    return result
+    return sequence_width(magr, x, y)
 
 
 def red_sequence_peak_gr(magr,
-        x=[-22.5, -21, -20, -19, -18, -15],
-        y=[0.95, 0.95, 0.8, 0.7, 0.7, 0.7]):
-    c2, c1, c0 = np.polyfit(x, np.log(y), deg=2)
-    result = np.exp(c0 + c1*magr + c2*magr**2)
-    xmax = np.max(x)
-    edge_value = np.exp(c0 + c1*xmax + c2*xmax**2)
-    return np.where(magr > xmax, edge_value, result)
+        x=[-25, -21, -20, -19, -18, -15],
+        y=[1.1, 0.95, 0.8, 0.7, 0.7, 0.7]):
+    return sequence_peak(magr, x, y)
 
 
 def main_sequence_peak_gr(magr,
-        x=[-22.5, -21, -20, -19, -18, -15],
+        x=[-25, -21, -20, -19, -18, -15],
         y=[0.65, 0.65, 0.6, 0.4, 0.4, 0.35]):
-    c2, c1, c0 = np.polyfit(x, np.log(y), deg=2)
-    return np.exp(c0 + c1*magr + c2*magr**2)
+    return sequence_peak(magr, x, y)
 
 
 def quiescent_fraction_gr(magr,
@@ -81,45 +88,25 @@ def g_minus_r(magr, seed=None):
 def red_sequence_width_ri(magr,
         x=[-22.5, -21, -20, -18, -15],
         y=[0.025, 0.03, 0.03, 0.025, 0.025]):
-    c2, c1, c0 = np.polyfit(x, y, deg=2)
-    result = c0 + c1*magr + c2*magr**2
-
-    #  Now clip endpoints
-    xmin, xmax = np.min(x), np.max(x)
-    faint_edge_value = c0 + c1*xmax + c2*xmax**2
-    bright_edge_value = c0 + c1*xmin + c2*xmin**2
-    result = np.where(magr > xmax, faint_edge_value, result)
-    result = np.where(magr < xmin, bright_edge_value, result)
-    return result
+    return sequence_width(magr, x, y)
 
 
 def main_sequence_width_ri(magr,
         x=[-22.5, -21, -20, -18, -15],
         y=[0.025, 0.065, 0.065, 0.06, 0.06]):
-    c2, c1, c0 = np.polyfit(x, y, deg=2)
-    result = c0 + c1*magr + c2*magr**2
-
-    #  Now clip endpoints
-    xmin, xmax = np.min(x), np.max(x)
-    faint_edge_value = c0 + c1*xmax + c2*xmax**2
-    bright_edge_value = c0 + c1*xmin + c2*xmin**2
-    result = np.where(magr > xmax, faint_edge_value, result)
-    result = np.where(magr < xmin, bright_edge_value, result)
-    return result
+    return sequence_width(magr, x, y)
 
 
 def red_sequence_peak_ri(magr,
-        x=[-22.5, -21, -20, -19.5, -19, -18, -15],
+        x=[-23, -21, -20, -19.5, -19, -18, -15],
         y=[0.435, 0.41, 0.4, 0.385, 0.375, 0.35, 0.31]):
-    c2, c1, c0 = np.polyfit(x, np.log(y), deg=2)
-    return np.exp(c0 + c1*magr + c2*magr**2)
+    return sequence_peak(magr, x, y)
 
 
 def main_sequence_peak_ri(magr,
-        x=[-22.5, -21, -20, -19, -18, -15],
+        x=[-25, -21, -20, -19, -18, -15],
         y=[0.4, 0.35, 0.3, 0.25, 0.215, 0.185]):
-    c2, c1, c0 = np.polyfit(x, np.log(y), deg=2)
-    return np.exp(c0 + c1*magr + c2*magr**2)
+    return sequence_peak(magr, x, y)
 
 
 def quiescent_fraction_ri(magr,
