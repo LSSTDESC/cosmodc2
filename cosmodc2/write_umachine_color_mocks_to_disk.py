77ed35f9ca995b2492394d2189c537e8ec18608c
Andrew Hearin <andrew.hearin@yale.edu>
2018-04-05 09:47:56 -0500

    Ported assign_restframe_sdss_gri function into write_snapshot_mocks_to_disk

--- cosmodc2/write_umachine_color_mocks_to_disk.py
+++ cosmodc2/write_umachine_color_mocks_to_disk.py
@@ -4,8 +4,7 @@ import numpy as np
 from time import time
 from astropy.table import Table
 from cosmodc2.sdss_colors import load_umachine_processed_sdss_catalog
-from cosmodc2.sdss_colors import mock_magr, gr_ri_monte_carlo, remap_cluster_bcg_gr_ri_color
-from cosmodc2.size_modeling import mc_size_vs_luminosity_early_type, mc_size_vs_luminosity_late_type
+from cosmodc2.sdss_colors import assign_restframe_sdss_gri
 
 
 def write_snapshot_mocks_to_disk(sdss_fname,
@@ -25,46 +24,25 @@ def write_snapshot_mocks_to_disk(sdss_fname,
         mock = Table.read(umachine_mock_fname, path='data')
         sdss = load_umachine_processed_sdss_catalog(sdss_fname)
 
-        #  Assign r-band magnitude
-        mock['restframe_extincted_sdss_abs_magr'] = mock_magr(
-                    mock['upid'], mock['obs_sm'], mock['sfr_percentile'],
-                    mock['host_halo_mvir'], sdss['sm'], sdss['sfr_percentile_fixed_sm'],
-                    sdss['restframe_extincted_sdss_abs_magr'], sdss['z'])
+        upid_mock = mock['upid']
+        mstar_mock = mock['obs_sm']
+        sfr_percentile_mock = mock['sfr_percentile']
+        host_halo_mvir_mock = mock['host_halo_mvir']
+        redshift_mock = np.zeros(len(mock)) + 0.0
 
-        #  Assign g-r & r-i colors
-        magr = mock['restframe_extincted_sdss_abs_magr']
-        percentile = mock['sfr_percentile']
-        redshift = np.zeros_like(magr)
+        logmstar_sdss = sdss['sm']
+        sfr_percentile_sdss = sdss['sfr_percentile_fixed_sm']
+        sdss_magr = sdss['restframe_extincted_sdss_abs_magr']
+        sdss_redshift = sdss['z']
 
-        gr, ri, is_red_ri, is_red_gr = gr_ri_monte_carlo(
-            magr, percentile, redshift, local_random_scale=0.1)
+        magr, gr_mock, ri_mock = assign_restframe_sdss_gri(
+            upid_mock, mstar_mock, sfr_percentile_mock, host_halo_mvir_mock, redshift_mock,
+            logmstar_sdss, sfr_percentile_sdss, sdss_magr, sdss_redshift)
+        mock['restframe_extincted_sdss_abs_magr'] = magr
+        mock['restframe_extincted_sdss_gr'] = gr_mock
+        mock['restframe_extincted_sdss_ri'] = ri_mock
 
-        mock['restframe_extincted_sdss_gr'] = gr
-        mock['restframe_extincted_sdss_ri'] = ri
-        mock['is_red_gr'] = is_red_gr
-        mock['is_red_ri'] = is_red_ri
-
-        _result = remap_cluster_bcg_gr_ri_color(
-                mock['upid'], mock['host_halo_mvir'],
-                np.copy(mock['restframe_extincted_sdss_gr']),
-                np.copy(mock['restframe_extincted_sdss_ri']),
-                mock['is_red_gr'], mock['is_red_ri'])
-        gr_remapped, ri_remapped, is_red_gr_remapped, is_red_ri_remapped = _result
-
-        mock['_gr_no_remap'] = np.copy(mock['restframe_extincted_sdss_gr'])
-        mock['_ri_no_remap'] = np.copy(mock['restframe_extincted_sdss_ri'])
-        mock['restframe_extincted_sdss_gr'] = gr_remapped
-        mock['restframe_extincted_sdss_ri'] = ri_remapped
-        mock['_is_red_ri_no_remap'] = np.copy(is_red_ri)
-        mock['_is_red_gr_no_remap'] = np.copy(is_red_gr)
-        mock['is_red_gr'] = is_red_gr_remapped
-        mock['is_red_ri'] = is_red_ri_remapped
-
-        #  Assign bulge and disk sizes
-        mock['bulge_size'] = mc_size_vs_luminosity_early_type(
-                mock['restframe_extincted_sdss_abs_magr'], redshift)
-        mock['disk_size'] = mc_size_vs_luminosity_late_type(
-                mock['restframe_extincted_sdss_abs_magr'], redshift)
+        raise NotImplementedError("Still need to GalSample into AlphaQ")
 
         print("          Writing to disk using commit hash {}".format(commit_hash))
         mock.meta['cosmodc2_commit_hash'] = commit_hash
