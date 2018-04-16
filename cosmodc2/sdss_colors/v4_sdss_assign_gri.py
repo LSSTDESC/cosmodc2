a108778eb7f411e60c9c43a4784f78e526ed8a2d
Andrew Hearin <andrew.hearin@yale.edu>
2018-04-16 11:49:50 -0500

    Finished migrating new magr assignment into demo_protoDC2_v4 notebook

--- cosmodc2/sdss_colors/v4_sdss_assign_gri.py
+++ cosmodc2/sdss_colors/v4_sdss_assign_gri.py
@@ -1,27 +1,24 @@
 """
 """
 import numpy as np
-from ..sdss_colors import mock_magr, gr_ri_monte_carlo
+from ..sdss_colors import magr_monte_carlo, gr_ri_monte_carlo
 from ..sdss_colors import remap_cluster_bcg_gr_ri_color, remap_cluster_satellite_gr_ri_color
+
 
 __all__ = ('assign_restframe_sdss_gri', )
 
 
 def assign_restframe_sdss_gri(upid_mock, mstar_mock, sfr_percentile_mock,
-            host_halo_mvir_mock, redshift_mock,
-            logmstar_sdss, sfr_percentile_sdss, sdss_magr, sdss_redshift):
+            host_halo_mvir_mock, redshift_mock):
     """
     """
     ngals = len(upid_mock)
-    magr = mock_magr(
-            upid_mock, mstar_mock, sfr_percentile_mock, host_halo_mvir_mock,
-            logmstar_sdss, sfr_percentile_sdss, sdss_magr, sdss_redshift)
 
-    # percentile = mock['sfr_percentile']
-    # redshift = np.zeros_like(magr)
     redshift_mock = np.atleast_1d(redshift_mock)
     if len(redshift_mock) == 1:
         redshift_mock = np.zeros(ngals).astype('f4') + redshift_mock[0]
+
+    magr = magr_monte_carlo(mstar_mock, redshift_mock)
 
     gr_mock, ri_mock, is_red_ri_mock, is_red_gr_mock = gr_ri_monte_carlo(
         magr, sfr_percentile_mock, redshift_mock, local_random_scale=0.1)
