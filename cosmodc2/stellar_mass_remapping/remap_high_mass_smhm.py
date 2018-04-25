295d6df5cb380364b7e3d2dc09548b4b73290f6a
Andrew Hearin <andrew.hearin@yale.edu>
2018-04-25 07:28:53 -0500

    Finished porting mstar boost to python module

--- cosmodc2/stellar_mass_remapping/remap_high_mass_smhm.py
+++ cosmodc2/stellar_mass_remapping/remap_high_mass_smhm.py
@@ -7,9 +7,13 @@ from scipy.stats import binned_statistic
 __all__ = ('lift_high_mass_mstar', )
 
 
-def lift_high_mass_mstar(mpeak, mstar, boosted_high_mass_slope, logmpeak_pivot):
+def lift_high_mass_mstar(mpeak, mstar, redshift,
+        z_table=[0.35, 0.5, 1], slope_table=[0.5, 0.65, 0.8], pivot_table=[13.25, 13, 12.5]):
     """
     """
+    logmpeak_pivot = calculate_logmpeak_pivot(redshift, z_table, pivot_table)
+    boosted_high_mass_slope = calculate_high_mass_slope(redshift, z_table, slope_table)
+
     high_mass_slope, high_mass_intercept = fit_high_mass_smhm(mpeak, mstar)
 
     boosted_high_mass_intercept = calculate_boosted_new_intercept(
@@ -17,6 +21,18 @@ def lift_high_mass_mstar(mpeak, mstar, boosted_high_mass_slope, logmpeak_pivot):
 
     return new_mstar(mpeak, mstar, logmpeak_pivot,
                 boosted_high_mass_intercept, boosted_high_mass_slope)
+
+
+def calculate_high_mass_slope(redshift, z_table, slope_table):
+    """
+    """
+    return np.interp(redshift, z_table, slope_table)
+
+
+def calculate_logmpeak_pivot(redshift, z_table, pivot_table):
+    """
+    """
+    return np.interp(redshift, z_table, pivot_table)
 
 
 def fit_high_mass_smhm(mpeak, mstar, logmpeak_low=12.5, logmpeak_high=14.5):
