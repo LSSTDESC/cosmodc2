553a7b403a15b315d15bd20950c669d2a2f512f2
Andrew Hearin <andrew.hearin@yale.edu>
2018-04-29 20:08:47 -0500

    Changed API of lift_high_mass_mstar to accept upid argument so that only central galaxy M* are lifted at high Mpeak

--- cosmodc2/stellar_mass_remapping/remap_high_mass_smhm.py
+++ cosmodc2/stellar_mass_remapping/remap_high_mass_smhm.py
@@ -13,11 +13,13 @@ def redshift_lifting_probability(redshift, z_low=0.25, z_high=0.5):
     return np.interp(redshift, [z_low, z_high], [0., 1.])
 
 
-def lift_high_mass_mstar(mpeak, mstar, redshift,
+def lift_high_mass_mstar(mpeak, mstar, upid, redshift,
         z_table=[0.25, 0.5, 1], slope_table=[0.5, 0.65, 0.7], pivot_table=[13.25, 13, 12.5]):
     """
     """
     lifting_probability = redshift_lifting_probability(redshift)
+    lifting_probability[upid != -1] = 0.
+
     lifting_mask = np.random.rand(len(lifting_probability)) < lifting_probability
 
     logmpeak_pivot = calculate_logmpeak_pivot(redshift, z_table, pivot_table)
