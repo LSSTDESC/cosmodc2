334aae6abff62fb58a66fec1c8ef3d37ac13b911
Andrew Hearin <andrew.hearin@yale.edu>
2018-04-29 19:04:58 -0500

    Now only lifting stellar mass for galaxies with z>0.5, which improves clustering somewhat

--- cosmodc2/stellar_mass_remapping/remap_high_mass_smhm.py
+++ cosmodc2/stellar_mass_remapping/remap_high_mass_smhm.py
@@ -7,10 +7,19 @@ from scipy.stats import binned_statistic
 __all__ = ('lift_high_mass_mstar', )
 
 
+def redshift_lifting_probability(redshift, z_low=0.25, z_high=0.5):
+    """
+    """
+    return np.interp(redshift, [z_low, z_high], [0., 1.])
+
+
 def lift_high_mass_mstar(mpeak, mstar, redshift,
         z_table=[0.25, 0.5, 1], slope_table=[0.5, 0.65, 0.7], pivot_table=[13.25, 13, 12.5]):
     """
     """
+    lifting_probability = redshift_lifting_probability(redshift)
+    lifting_mask = np.random.rand(len(lifting_probability)) < lifting_probability
+
     logmpeak_pivot = calculate_logmpeak_pivot(redshift, z_table, pivot_table)
     boosted_high_mass_slope = calculate_high_mass_slope(redshift, z_table, slope_table)
 
@@ -19,8 +28,12 @@ def lift_high_mass_mstar(mpeak, mstar, redshift,
     boosted_high_mass_intercept = calculate_boosted_new_intercept(
             high_mass_intercept, high_mass_slope, boosted_high_mass_slope, logmpeak_pivot)
 
-    return new_mstar(mpeak, mstar, logmpeak_pivot,
+    lifted_mstar = new_mstar(mpeak, mstar, logmpeak_pivot,
                 boosted_high_mass_intercept, boosted_high_mass_slope)
+
+    result = np.copy(mstar)
+    result[lifting_mask] = lifted_mstar[lifting_mask]
+    return result
 
 
 def calculate_high_mass_slope(redshift, z_table, slope_table):
