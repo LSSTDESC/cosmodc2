4953d269105aedc2d4d2d2750e343852022de926
Andrew Hearin <andrew.hearin@yale.edu>
2018-05-03 11:12:56 -0500

    Added v4_paint_colors_onto_umachine_snaps_temp to experiment with

--- cosmodc2/sdss_colors/v4_remap_umachine_snaps.py
+++ cosmodc2/sdss_colors/v4_remap_umachine_snaps.py
@@ -5,7 +5,7 @@ from .analytical_magr import magr_monte_carlo
 from .analytical_colors import gr_ri_monte_carlo
 
 
-__all__ = ('v4_paint_colors_onto_umachine_snaps', )
+__all__ = ('v4_paint_colors_onto_umachine_snaps', 'v4_paint_colors_onto_umachine_snaps_temp')
 
 
 def v4_paint_colors_onto_umachine_snaps(mpeak, mstar, upid, redshift, sfr_percentile):
@@ -18,5 +18,19 @@ def v4_paint_colors_onto_umachine_snaps(mpeak, mstar, upid, redshift, sfr_percen
 
     gr_mock, ri_mock, is_red_ri_mock, is_red_gr_mock = gr_ri_monte_carlo(
         new_magr_rest, sfr_percentile, redshift, local_random_scale=0.1)
+
+    return new_mstar, new_magr_rest, gr_mock, ri_mock, is_red_ri_mock, is_red_gr_mock
+
+
+def v4_paint_colors_onto_umachine_snaps_temp(mpeak, mstar, upid, redshift, sfr_percentile, **kwargs):
+    """
+    """
+    new_mstar = lift_high_mass_mstar(mpeak, mstar, upid,  redshift)
+    new_mstar = new_mstar
+
+    new_magr_rest = magr_monte_carlo(new_mstar, upid, redshift)
+
+    gr_mock, ri_mock, is_red_ri_mock, is_red_gr_mock = gr_ri_monte_carlo(
+        new_magr_rest, sfr_percentile, redshift, local_random_scale=0.1, **kwargs)
 
     return new_mstar, new_magr_rest, gr_mock, ri_mock, is_red_ri_mock, is_red_gr_mock
