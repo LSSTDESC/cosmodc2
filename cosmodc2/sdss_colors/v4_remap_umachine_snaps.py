00e02fa4fa5f92f810ed91538dbec56c4f85eb4a
Andrew Hearin <andrew.hearin@yale.edu>
2018-05-04 11:08:53 -0500

    Rewrote v4_paint_colors_onto_umachine_snaps_temp function so that it calls the newly corrected assign_restframe_sdss_gri function

--- cosmodc2/sdss_colors/v4_remap_umachine_snaps.py
+++ cosmodc2/sdss_colors/v4_remap_umachine_snaps.py
@@ -3,6 +3,7 @@
 from ..stellar_mass_remapping import lift_high_mass_mstar
 from .analytical_magr import magr_monte_carlo
 from .analytical_colors import gr_ri_monte_carlo
+from .v4_sdss_assign_gri import assign_restframe_sdss_gri
 
 
 __all__ = ('v4_paint_colors_onto_umachine_snaps', 'v4_paint_colors_onto_umachine_snaps_temp')
@@ -12,7 +13,6 @@ def v4_paint_colors_onto_umachine_snaps(mpeak, mstar, upid, redshift, sfr_percen
     """
     """
     new_mstar = lift_high_mass_mstar(mpeak, mstar, upid,  redshift)
-    new_mstar = new_mstar
 
     new_magr_rest = magr_monte_carlo(new_mstar, upid, redshift)
 
@@ -22,15 +22,14 @@ def v4_paint_colors_onto_umachine_snaps(mpeak, mstar, upid, redshift, sfr_percen
     return new_mstar, new_magr_rest, gr_mock, ri_mock, is_red_ri_mock, is_red_gr_mock
 
 
-def v4_paint_colors_onto_umachine_snaps_temp(mpeak, mstar, upid, redshift, sfr_percentile, **kwargs):
+def v4_paint_colors_onto_umachine_snaps_temp(
+        mpeak, mstar, upid, redshift, sfr_percentile, host_halo_mvir, **kwargs):
     """
     """
-    new_mstar = lift_high_mass_mstar(mpeak, mstar, upid,  redshift)
-    new_mstar = new_mstar
+    new_mstar = lift_high_mass_mstar(mpeak, mstar, upid, redshift)
 
-    new_magr_rest = magr_monte_carlo(new_mstar, upid, redshift)
-
-    gr_mock, ri_mock, is_red_ri_mock, is_red_gr_mock = gr_ri_monte_carlo(
-        new_magr_rest, sfr_percentile, redshift, local_random_scale=0.1, **kwargs)
+    result = assign_restframe_sdss_gri(upid, new_mstar, sfr_percentile,
+                host_halo_mvir, redshift, **kwargs)
+    new_magr_rest, gr_mock, ri_mock, is_red_gr_mock, is_red_ri_mock = result
 
     return new_mstar, new_magr_rest, gr_mock, ri_mock, is_red_ri_mock, is_red_gr_mock
