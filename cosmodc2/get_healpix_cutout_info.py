a8ac0e51a20a2b723dc41e97ac3089db9a4e0598
Eve Kovacs <ekovacs@cooleylogin2.fst.alcf.anl.gov>
2018-06-25 19:36:04 +0000

    minor update to check for missing snapshots

--- cosmodc2/get_healpix_cutout_info.py
+++ cosmodc2/get_healpix_cutout_info.py
@@ -12,6 +12,10 @@ def get_healpix_cutout_info(pkldirname, infile, sim_name='AlphaQ', pklname='{}_z
         h5file = h5py.File(infile, 'r')
         snapshots = [str(k) for k in sorted(h5file.keys())][::-1]  #reverse order
         redshifts = [key for key in sorted(z2ts.keys()) if str(z2ts[key]) in snapshots]
+        available_snapshots = [str(z2ts[key]) for key in sorted(z2ts.keys()) if (key <= max(redshifts) and key >= min(redshifts))]
+        missing_snapshots = [s for s in available_snapshots if not s in snapshots]
+        if missing_snapshots:
+            print('{} is missing snapshots {}'.format(os.path.basename(infile), ', '.join(missing_snapshots)))
     else:
         print('{} not found'.format(infile))
 
