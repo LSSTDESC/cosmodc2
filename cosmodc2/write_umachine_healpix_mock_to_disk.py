bbb468b9cd0ab766611a019eb1705f654696e6ac
Andrew Hearin <ahearin@anl.gov>
2018-11-15 11:49:50 -0600

    Implemented assert statement to ensure no clusters have stellar mass greater than 10**13

--- cosmodc2/write_umachine_healpix_mock_to_disk.py
+++ cosmodc2/write_umachine_healpix_mock_to_disk.py
@@ -559,6 +559,10 @@ def build_output_snapshot_mock(
         dc2['obs_sm'][ultra_high_mvir_halo_mask] = mstar_array*(mhalo_ratio**0.5)
         dc2['restframe_extincted_sdss_abs_magr'][ultra_high_mvir_halo_mask] = magr_monte_carlo(
             dc2['obs_sm'][ultra_high_mvir_halo_mask], upid_array, redshift_array)
+        idx = np.argmax(dc2['obs_sm'])
+        halo_id_most_massive = dc2['halo_id'][idx]
+        assert dc2['obs_sm'][idx] < 10**13, "halo_id = {0} has stellar mass {1:.3e}".format(
+            halo_id_most_massive, dc2['obs_sm'][idx])
 
     dc2['x'] = dc2['target_halo_x'] + dc2['host_centric_x']
     dc2['vx'] = dc2['target_halo_vx'] + dc2['host_centric_vx']
